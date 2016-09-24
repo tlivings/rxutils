e1c948fcbd54ee01c10fde05cf010df3517e90c6
Trevor Livingston <tlivings@gmail.com>
2016-09-23 21:08:16 -0500

    use echo stream commonly.

--- test/test-netstringsubject.js
+++ test/test-netstringsubject.js
@@ -2,27 +2,7 @@
 
 const Test = require('tape');
 const NetstringSubject = require('../lib/netstringsubject');
-const DuplexStream = require('stream').Duplex;
-
-class EchoStream extends DuplexStream {
-    constructor(options) {
-        super(options);
-
-        this._pending = [];
-    }
-
-    _read() {
-        while (this._pending.length > 0) {
-            this.push(this._pending.shift());
-        }
-    }
-
-    _write(data, enc, callback) {
-        this._pending.push(data);
-
-        callback();
-    }
-}
+const EchoStream = require('./fixtures/echostream');
 
 Test.only('test netstring stream subject', (t) => {
 
